e2368d6229157021ba6888c5495d3cd40d42e172
SkyTu <1336923451@qq.com>
2024-10-30 21:01:40 +0800

    debug 7

--- fss/dcf/gpu_sgd.cu
+++ fss/dcf/gpu_sgd.cu
@@ -36,7 +36,7 @@ namespace dcf
         if (i < N)
         {
             C[i] = (A[i] << shift) + alpha * B[i];
-            if(i == 1) printf("%u %u %u %u %d\n", A[i], B[i], alpha, C[i], shift);
+            if(i == 0) printf("A[0] = %u B[0] = %u alpha = %u C[0] = %u shift = %d\n", A[i], B[i], alpha, C[i], shift);
         }
     }
 
