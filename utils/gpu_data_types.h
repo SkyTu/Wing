34dc1eaee8457b3416cf1848e6e71ec860578d58
SkyTu <1336923451@qq.com>
2024-12-23 23:36:52 +0800

    reset

--- utils/gpu_data_types.h
+++ utils/gpu_data_types.h
@@ -83,6 +83,6 @@ namespace wing
         static const int bw = 64;
         static const int scale = 24;
         static const int iteration_threshold = 0;
-        static const int extra_shift = 3;
+        static const int extra_shift = 0;
     }
 }
